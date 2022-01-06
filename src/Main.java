42758e1ca740e0ab61400c6427fa3d9da131e657
Timitriy <nanoonlik@gmail.com>
2022-01-06 14:47:32 +0300

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double growth = 1.70;
-        double weight = 60;
         double massIndex = service.calculate(60,1.70);
         System.out.println(massIndex);
     }
